5ad13bd27105271d1356d612895e1fb4ead57eea
Loup Quehec <lquehec@f2r9s10.clusters.42paris.fr>
2025-05-23 19:08:33 +0200

    p1

--- p1/scripts/init-master-node.sh
+++ p1/scripts/init-master-node.sh
@@ -18,9 +18,15 @@ fi
 # Get interface name
 iface=$(ip -o -4 addr show | grep "$MASTER_IP" | awk '{print $2}')
 # Install K3s
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--flannel-iface $iface" sh -
+curl -sfL https://get.k3s.io | \
+	INSTALL_K3S_EXEC="--flannel-iface $iface" \
+	K3S_KUBECONFIG_MODE="644" \
+	sh -
 # Wait for K3s to be ready
-sudo chmod +x /etc/rancher/k3s/k3s.yaml
+sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+# Create alias like in the subject
+echo "alias k='kubectl'" >> /home/vagrant/.bashrc
+# Share token to slaves
 K3S_NODE_TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
 echo "K3S_NODE_TOKEN=$K3S_NODE_TOKEN"
 echo "$K3S_NODE_TOKEN" > "${SHARED_FOLDER}/k3s_node_token"
