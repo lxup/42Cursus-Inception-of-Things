dbcd798a2af20ed6ab768da1a36bff44c6acff0b
Loup Quehec <lquehec@f2r9s10.clusters.42paris.fr>
2025-05-23 19:45:33 +0200

    p2

---
+++ p2/scripts/init-master-node.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Install dependencies
+sudo apt-get update
+if ! command -v curl &> /dev/null
+then
+	echo "curl could not be found, installing..."
+	sudo apt-get install -y curl
+fi
+
+# Get interface name
+iface=$(ip -o -4 addr show | grep "$MASTER_IP" | awk '{print $2}')
+# Install K3s
+curl -sfL https://get.k3s.io | \
+	INSTALL_K3S_EXEC="--flannel-iface $iface" \
+	K3S_KUBECONFIG_MODE="644" \
+	sh -
+# Wait for K3s to be ready
+sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+# Create alias like in the subject
+echo "alias k='kubectl'" >> /home/vagrant/.bashrc
+
