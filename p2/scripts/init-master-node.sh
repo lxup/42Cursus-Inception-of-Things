ed13b85be4eb582dc996104bbe436355f2264a17
Loup Quehec <lquehec@f2r8s14.clusters.42paris.fr>
2025-05-26 20:15:02 +0200

    p2

--- p2/scripts/init-master-node.sh
+++ p2/scripts/init-master-node.sh
@@ -16,15 +16,23 @@ curl -sfL https://get.k3s.io | \
 	INSTALL_K3S_EXEC="--flannel-iface $iface" \
 	K3S_KUBECONFIG_MODE="644" \
 	sh -
-# Wait for K3s to be ready
+
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 # Create alias like in the subject
 echo "alias k='kubectl'" >> /home/vagrant/.bashrc
 
-echo "Waiting node $MASTER..."
-kubectl wait --for=condition=Ready node/"$MASTER"
-echo "Node $MASTER ready !"
+# Wait for K3s nodes to be ready
+echo "Waiting for node to register..."
+until kubectl get nodes --no-headers 2>/dev/null | grep -q .; do
+  sleep 1
+done
 
+NODE_NAME=$(kubectl get nodes -o jsonpath='{.items[0].metadata.name}')
+echo "Waiting node $NODE_NAME..."
+kubectl wait --for=condition=Ready node/"$NODE_NAME"
+echo "Node $NODE_NAME ready !"
+
+kubectl apply -f "$CONF_FOLDER/namespaces.yaml"
 kubectl apply -f "$CONF_FOLDER/deployments.yaml"
-kubectl apply -f "$CONF_FOLDER/service.yaml"
+kubectl apply -f "$CONF_FOLDER/services.yaml"
 kubectl apply -f "$CONF_FOLDER/ingress.yaml"
